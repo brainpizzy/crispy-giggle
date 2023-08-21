d90064ad7243aaef3f870fc156ad658718a2dabe
brainpizzy <142627674+brainpizzy@users.noreply.github.com>
2023-08-21 06:53:41 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,15 @@
 var displayValue = "";
 
 function Display(value) {
-    displayValue += value;
+    const lastChar = displayValue.slice(-1);
+    const operators = ['+', '-', '*', '/'];
+
+    if (operators.includes(lastChar) && operators.includes(value)) {
+        displayValue = displayValue.slice(0, -1) + value;
+    } else {
+        displayValue += value;
+    }
+
     document.getElementById("display").value = displayValue;
 }
 
@@ -11,16 +19,17 @@ function clearDisplay() {
 }
 
 function calculateResult() {
-    const displayElement =document.getElementById("display");
+    const displayElement = document.getElementById("display");
 
     if (displayValue != "") {
-    try {
-        displayValue = eval(displayValue);
-        document.getElementById("display").value = displayValue;
-    } catch (error) {
-        document.getElementById("display").value = "Dude,that's an error!";
-       }
+        try {
+            displayValue = eval(displayValue);
+            document.getElementById("display").value = displayValue;
+        } catch (error) {
+            document.getElementById("display").value = "Dude, that's an error!";
+        }
     } else {
         displayElement.value = "";
     }
 }
+
